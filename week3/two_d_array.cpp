450ebe9c9f59fa988ba0506806aaeafa9e9f7924
Marquis Bryant <bryantml@vcu.edu>
2019-02-02 01:24:38 -0500

    Create two_d_array.cpp

---
+++ week3/two_d_array.cpp
@@ -0,0 +1,27 @@
+// two_d_array.cpp : Complete the following program to display the contents of the 2-d array
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+int main() {
+	char t_d_array[5][7] = {
+	 {' ','/','\\','_','/','\\',' '},
+	 {'/','','D','','D','','\\'},
+	 {'(','=','=','O','=','=',')'},
+	 {'\\',' ',' ','~',' ',' ','/'},
+	 {' ','-','-','-','-','-',' '},
+	};
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
