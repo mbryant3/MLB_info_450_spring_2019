6821129b68c5c4759320901dc2f5e6465791b514
Marquis Bryant <bryantml@vcu.edu>
2019-02-11 23:47:54 -0500

    Update week4program2.cpp

--- week4/week4program2.cpp
+++ week4/week4program2.cpp
@@ -21,7 +21,7 @@ int main()
 	{
 		for (int l = 0; l < 6; l++)
 		{
-			cout << "Enter in Student's Name" << endl;
+			cout << "Enter in Student's Name" << endl; // where the user input happens
 			getline(cin, name);
 			if (name.empty())
 			{
@@ -31,10 +31,10 @@ int main()
 				for (int i = 0; i < 5; i++)
 				{
 					for (int j = 0; j < 6; j++)
-					{
+					{                                            // checks if string is empty
 						if (!ClassList[i][j].empty())
 						{
-							cout << ClassList[i][l];
+							cout << ClassList[f][l];
 							cout << "\n";
 						}
 						else
@@ -45,25 +45,21 @@ int main()
 					}
 			
 				}
-		
-			
-				
-			
 			}
 			else {
 				ClassList[f][l] = name;
 			}
 		}
-	}
-	int x = 0;
-	int y = 0;
-	for (x = 0; x < 5; x++)
+	} // where the list is printed at 
+	int first = 0;
+	int last = 0;
+	for (first = 0; first < 5; first++)
 	{
-		for (y = 0; y < 6; y++) 
+		for (last = 0; last < 6; last++) 
 		{
-			if (!ClassList[x][y].empty())
+			if (!ClassList[first][last].empty())
 			{
-				cout << ClassList[x][y];
+				cout << ClassList[first][last];
 				cout << "\n";
 			}
 			else {
@@ -75,14 +71,3 @@ int main()
 
 	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
